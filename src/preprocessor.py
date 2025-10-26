6290c5ab2610553f60c2c4304675a8d5f60e7da4
Egor Maksimov <E@rand.ru>
2025-10-26 18:54:55 +0300

    Исправлена ошибка при предобработке имён файлов, начинающихся с - в функции preprocess_command

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -9,16 +9,21 @@ def preprocess_command(s: str) -> list[str | Path]:
         :return: Возвращает предобработанную команду
     """
 
-    parsed_command_and_args: list[str] = []
+    parsed_command_and_args: list[tuple[str, str]] = []
     current_position = 0
     buffer = ""
     quoted = ""
     screened = False
+    buffer_data_type = "OPTION"
     while current_position < len(s):
         if len(quoted) == 0 and s[current_position] == " " and not screened:
-            parsed_command_and_args.append(buffer)
+            if buffer[0] != "-" and len(buffer) > 1:
+                buffer_data_type = "PATH"
+            parsed_command_and_args.append((buffer, buffer_data_type))
             buffer = ""
+            buffer_data_type = "OPTION"
         elif (s[current_position] == "'" or s[current_position] == '"') and not screened:
+            buffer_data_type = "PATH"
             if quoted == s[current_position]:
                 quoted = ""
             else:
@@ -27,23 +32,26 @@ def preprocess_command(s: str) -> list[str | Path]:
                 else:
                     buffer += s[current_position]
         elif s[current_position] == "\\" and not screened:
+            buffer_data_type = "PATH"
             screened = True
         else:
             buffer += s[current_position]
             screened = False
         current_position += 1
     if len(buffer) != 0:
-        parsed_command_and_args.append(buffer)
+        if buffer[0] != "-" and len(buffer) > 1:
+            buffer_data_type = "PATH"
+        parsed_command_and_args.append((buffer, buffer_data_type))
     if len(quoted) != 0:
         return ["ERROR: unclosed quote"]
 
-    command_and_correct_paths: list[str | Path] = [parsed_command_and_args[0]]
+    command_and_correct_paths: list[str | Path] = [parsed_command_and_args[0][0]]
     if len(parsed_command_and_args) > 1:
         for arg in parsed_command_and_args[1:]:
-            if arg[0] == "-" and len(arg) > 1:
-                command_and_correct_paths.extend(["-" + option for option in arg[1:]])
+            if arg[1] == "OPTION":
+                command_and_correct_paths.extend(["-" + option for option in arg[0][1:]])
                 continue
             else:
-                command_and_correct_paths.append(Path(arg).expanduser().resolve())
+                command_and_correct_paths.append(Path(arg[0]).expanduser().resolve())
 
     return command_and_correct_paths
